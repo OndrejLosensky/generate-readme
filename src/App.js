cb9cb5246e4e6e690fdb4403e663621b8c0a3939
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-17 21:54:26 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { saveAs } from 'file-saver';
+import DOMPurify from 'dompurify';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+      <h1 className="text-5xl"> README GENERATOR</h1>
+   </div>
   );
 }
 
