5a782e28f750e19e77c9fbe97d2b21502a0eaa91
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-17 23:04:25 +0200

    přidání více inputů

--- src/components/createReadme/CreateReadme.js
+++ src/components/createReadme/CreateReadme.js
@@ -8,7 +8,13 @@ import AddInput from '../input/AddInput';
 function CreateReadme() {
   const [title, setTitle] = useState('');
   const [shortDescription, setShortDescription] = useState('');
-  // Přidat další UseState pro každý Input do Formu
+  const [author, setAuthor] = useState('');
+  const [version1, setVersion1] = useState('');
+  const [date, setDate] = useState('');
+  const [license, setLicense] = useState('');
+  const [contact, setContact] = useState('');
+
+
   const [generatedReadme, setGeneratedReadme] = useState('');
 
   const generateReadme = () => {
@@ -19,7 +25,6 @@ function CreateReadme() {
     `;
 
     // Stáhne soubor s názvem README.MD 
-
     //const blob = new Blob([readmeContent], { type: 'text/markdown' });
     //saveAs(blob, 'README.md');
 
@@ -29,25 +34,85 @@ function CreateReadme() {
 
   return (
     <div>
-      <h1 className='uppercase text-4xl font-semibold text-center text-white py-6'> Readme generator</h1>
+      <h1 className='uppercase text-4xl font-bold text-center text-white py-6'> Readme generator</h1>
       <div className='flex flex-col items-center  h-screen'>
-          <div className='w-[40%] h-[80%] bg-red-800 rounded-2xl'>
+          <div className='w-[40%] h-[85%] bg-red-800 rounded-2xl'>
             <form className='flex flex-col items-center py-8'>
               <div className='py-6'>
+                
                 <Title text="Název projektu:"/>
-                <AddInput
-                  placeholder="Zde zadejte název"
-                  value={title}
-                  onChange={(e) => setTitle(e.target.value)}
-                />
-                <Title text="Krátký popisek:"/>
-                <AddInput
-                  placeholder="krátký popisek"
-                  value={shortDescription}
-                  onChange={(e) => setShortDescription(e.target.value)}
-                />
+                <div className='w-[450px] rounded-md'>
+                  <AddInput
+                    placeholder="Zde zadejte název"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+                </div>
+
+                <div>
+                  <Title text="Krátký popisek:"/>
+                  <AddInput
+                    placeholder="krátký popisek"
+                    value={shortDescription}
+                    onChange={(e) => setShortDescription(e.target.value)}
+                  />
+                </div>
+                {/* TŘETÍ ŘÁDEK */}
+                <div className='flex flex-row'>
+                  <div className='w-[46%]'>
+                    <Title text="Autor"/>
+                    <AddInput
+                    placeholder="vaše jméno"
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                    />
+                  </div>
+                  <div className='w-[8%]'></div>
+                  <div className='w-[46%]'>
+                    <Title text="Verze"/>
+                    <AddInput
+                    placeholder="Aktuální verze"
+                    value={version1}
+                    onChange={(e) => setVersion1(e.target.value)}
+                    />
+                  </div>
+                </div>
+                {/* ČTVRTÝ ŘÁDEK */}
+                <div className=' flex flex-row'>
+                  <div className='w-[46%]'>
+                    <Title text="Datum"/>
+                    <AddInput
+                    placeholder="Datum"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    />
+                  </div>
+                  <div className='w-[8%]'></div>
+                  <div className='w-[46%]'>
+                    <Title text="Licence"/>
+                    <AddInput
+                    placeholder="Typ licence"
+                    value={license}
+                    onChange={(e) => setLicense (e.target.value)}
+                    />
+                  </div>
+                </div>
+
+                {/* Pátý řádek*/}
+                <div>
+                  <Title text="Kontakt"/>
+                  <AddInput
+                  placeholder="Váš e-mail"
+                  value={contact}
+                  onChange={(e) => setContact(e.target.value)}
+                  />
+                </div>
+
+
               </div>
-              <div className='px-8'>
+
+              {/* Tlačítko*/}
+              <div className='px-8 py-6'>
                 <button className='text-white px-6 py-3 bg-blue-500 rounded-xl hover:bg-blue-400 duration-300'
                  type="button" onClick={generateReadme}>
                   Vygeneruj README
