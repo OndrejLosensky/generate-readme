42fd662d9a4d09138508e13d720a02e93b0b4a82
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-17 21:57:29 +0200

    připravení tvorby README souborů

--- src/App.js
+++ src/App.js
@@ -4,10 +4,38 @@ import DOMPurify from 'dompurify';
 
 
 function App() {
+  const [title, setTitle] = useState('');
+  // Přidat další UseState pro každý Input do Formu
+  const [generatedReadme, setGeneratedReadme] = useState('');
+
+  const generateReadme = () => {
+    // Šablona readme | lze nastavit vzled
+    const readmeContent = `<h1>${title}</h1>`;
+
+    // Stáhne soubor s názvem README.MD 
+    const blob = new Blob([readmeContent], { type: 'text/markdown' });
+    saveAs(blob, 'README.md');
+
+    // Zobrazí obsah readme v prohlížeči
+    setGeneratedReadme(DOMPurify.sanitize(readmeContent));
+  };
+
   return (
-   <div>
-      <h1 className="text-5xl"> README GENERATOR</h1>
-   </div>
+    <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Název projektu"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        
+        <button type="button" onClick={generateReadme}>
+          Generate README
+        </button>
+      </form>
+      <div dangerouslySetInnerHTML={{ __html: generatedReadme }}></div>
+    </div>
   );
 }
 
