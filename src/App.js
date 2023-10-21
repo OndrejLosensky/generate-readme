1bd7f8696bb3dc0e485eb4720081ca703b86b801
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-21 13:11:40 +0200

    animovaná sekce dokumentace

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, {useState} from 'react';
 import Home from './components/home/Home';
 import Documentation from './components/documentation/Documentation';
-function App() {
 
-  const [open, setOpen] = useState(false);
+function App() {
+  const [showDocumentation, setShowDocumentation] = useState(false);
 
   const toggleDocumentation = () => {
-    setOpen(!open);
-    
+    setShowDocumentation(!showDocumentation);
+  };
+
+  const documentationStyles = {
+    flex: 1,
+    width: '100%',
+    position: 'fixed',
+    top: 0,
+    bottom: 0,
+    zIndex: 2,
+    transform: `translateX(${showDocumentation ? 0 : '100%'})`,
+    transition: 'transform 0.5s ease',
+  };
+
+  const homeStyles = {
+    flex: 1,
+    display: 'flex',
+    transform: `translateX(${showDocumentation ? '-100%' : 0})`,
+    transition: 'transform 0.5s ease',
   };
 
   return (
-    <section className=''>
-      <div className={`transition-all ${open ? 'w-0' : 'w-full'} duration-500`} >
-        <Home openDocumentation={toggleDocumentation} />
-      </div>
-      {open && (
-        <div className={`transition-all w-full duration-500 bg-whiteText`} style={{ transform: open ? 'scale(1)' : 'scale(0)' }}>
+    <section style={{ display: 'flex', overflow: 'hidden' }}>
+      {showDocumentation && (
+        <div style={documentationStyles}>
           <button className='absolute left-0 top-0 pl-8 pt-8' onClick={toggleDocumentation}> Zavřít </button>
           <Documentation />
         </div>
       )}
+      <div style={homeStyles}>
+        <Home openDocumentation={toggleDocumentation} />
+      </div>
     </section>
-  )
+  );
 }
+
 
 export default App;
--- src/components/documentation/Documentation.js
+++ src/components/documentation/Documentation.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Documentation = () => {
     return(
-        <div>
-            <h2> Docs </h2>
+        <div className="flex flex-col items-center">
+            <h2 className="text-5xl font-bold pt-8 "> Docs </h2>
+            {/* sekce s popiskem */}
+            <div className="pt-4">
+                <p className="text-center">
+                    Vítejte v sekci dokumentace <br/>
+                    zde najdete jak tuto aplikaci používat
+                </p>
+            </div>
         </div>
     )
 }
