2983003e9058a860eb90f22142434cfc1c4a6f50
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-22 10:56:30 +0200

    scroll-indicator pro jiný project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
     return (
         <div className="w-full">
-            <h1 className="py-4 uppercase text-5xl font-bold text-center text-whiteText pt-8" style={{ position: 'fixed', top: 0, left: 0, width: '100%', background: '#333' }}>
+            <h1 className="py-4 uppercase text-5xl font-bold text-center text-whiteText pt-8" style={{ position: 'fixed', top: 0, left: 0, width: '100%', background: '#332F39' }}>
                 Readme generator
             </h1>
         </div>
