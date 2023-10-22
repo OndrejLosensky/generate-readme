ed64711affd73942c1f7572a32a59e9d684ad42e
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-22 13:22:43 +0200

    Nová componenta pro input

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,10 +13,10 @@ const Home = (props) => {
           <Header/>
           <ScrollProgressBar />
 
-          <div className="App h-screen flex justify-center items-center bg-black">
+          <div className="h-screen flex justify-center items-center bg-black">
             <label className='relative cursor-pointer'>
               <input type="text" placeholder="Input" className='h-20 w-96 px-6 text-4xl text-whiteText bg-darkBg border-white border-2 rounded-lg border-opacity-50 outline-none focus:border-buttonColor placeholder-copiedColor placeholder-opacity-0 transition duration-200' />
-              <span className='text-4xl text-whiteText text-opacity-80 bg-darkBg absolute left-5 top-5 px-1 transition duration-200 input-text'>Input</span>
+              <span className='text-4xl text-whiteText text-opacity-80 bg-darkBg absolute left-5 top-5 px-1 transition duration-200 input-text'> test </span>
             </label>
           </div>
 
