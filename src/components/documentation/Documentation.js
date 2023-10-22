37b9d0bc2513c452004cdd2d169c88473a78a02c
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-22 21:17:49 +0200

    animování textu

--- src/components/documentation/Documentation.js
+++ src/components/documentation/Documentation.js
@@ -1,6 +1,16 @@
 import React from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
+import { useEffect } from 'react';
 
 const Documentation = () => {
+
+     // ------------------- Animace -------------------
+    useEffect(() => {
+        AOS.init({duration: "1500" });
+    },[])
+  // -----------------------------------------------
+
     return(
         <div className=" flex flex-col items-center">
             <h2 className="text-5xl font-bold pt-2 uppercase "> dokumentace </h2>
@@ -12,10 +22,10 @@ const Documentation = () => {
                     jak kopírovat do vašeho readme souboru
                 </p>
                 <h3 className="pt-3 text-center text-3xl font-semibold"> Vložení dat do readme.md </h3>
-                <p> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 1 </span> vytvořte readme.md soubor ve vašem repository</p>
-                <p> <span className="text-right text-6xl font-black font-dancingScript pt-1"> 2 </span> vyplňte formulář v této aplikaci dle vašich potřeb</p>
-                <p> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 3 </span> klikněte na tlačítko kopírovat, které se nachází na spodní části</p>
-                <p> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 4 </span> vložte do vašeho README.md a obnovte stránku</p>
+                <p data-aos="fade-right"> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 1 </span> vytvořte readme.md soubor ve vašem repository</p>
+                <p data-aos="fade-right" data-aos-delay="500"> <span className="text-right text-6xl font-black font-dancingScript pt-1"> 2 </span> vyplňte formulář v této aplikaci dle vašich potřeb</p>
+                <p data-aos="fade-right" data-aos-delay="1000"> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 3 </span> klikněte na tlačítko kopírovat, které se nachází na spodní části</p>
+                <p data-aos="fade-right" data-aos-delay="1500"> <span className="text-left text-6xl font-black font-dancingScript pt-1"> 4 </span> vložte do vašeho README.md a obnovte stránku</p>
             </div>
         </div>
     )
