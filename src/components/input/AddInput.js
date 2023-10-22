a1582d2cba737786d28442b19138dc6ad23702ac
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-22 12:58:18 +0200

    uprava AddiInput

--- src/components/input/AddInput.js
+++ src/components/input/AddInput.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
+
 const AddInput = ({ placeholder, value, onChange, type }) => {
   return (
     <div className="mb-4">
-      <input
-        className="text-darkText bg-transparent border-2 border-darkColor rounded-md px-2 py-2 w-full"
-        type={type} // změnit na proměnnou, pokaždé nastavím typ, zda text nebo jiný
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-      />
+      <label>
+        <input
+          className=" bg-transparent border-2 border-darkColor rounded-md px-2 py-2 w-full"
+          type={type} // změnit na proměnnou, pokaždé nastavím typ, zda text nebo jiný
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
     </div>
   );
 };
