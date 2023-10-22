1697625b8403c3e9cad4edc92f1b386b9409df2b
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-22 13:15:22 +0200

    google-like Input animation

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,7 +3,7 @@ import CreateReadme from "../createReadme/CreateReadme";
 import Footer from "../footer/Footer"
 import Header from "../header/Header";
 import ScrollProgressBar from "../scrollProgressBar/ScrollProgressBar";
-
+import "../../index.css"
 
 const Home = (props) => {
     return(
@@ -12,6 +12,14 @@ const Home = (props) => {
         <div>
           <Header/>
           <ScrollProgressBar />
+
+          <div className="App h-screen flex justify-center items-center bg-black">
+            <label className='relative cursor-pointer'>
+              <input type="text" placeholder="Input" className='h-20 w-96 px-6 text-4xl text-whiteText bg-darkBg border-white border-2 rounded-lg border-opacity-50 outline-none focus:border-buttonColor placeholder-copiedColor placeholder-opacity-0 transition duration-200' />
+              <span className='text-4xl text-whiteText text-opacity-80 bg-darkBg absolute left-5 top-5 px-1 transition duration-200 input-text'>Input</span>
+            </label>
+          </div>
+
           <div className='text-whiteText text-xl text-center justify-center font-light pt-60 font-roboto'>
             <p> Vítejte v generátoru README.MD </p>
             <p> jednoduchým způsobem vytvořte minimalistické readme pro vaše projekty </p>
