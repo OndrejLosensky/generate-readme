7512903455d5cd41403543f3609ec4a6bffc046c
Ondrej Losensky <losenskyondrej@gmail.com>
2023-10-23 22:55:14 +0200

    assets upload

---
+++ src/Run.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ScrollProgressBar from "./components/scrollProgressBar/ScrollProgressBar";
+import App from "./App";
+
+function Run () {
+    return(
+        <section>
+            <ScrollProgressBar/>
+            <App/>
+        </section>
+    )
+
+}
+
+export default Run
